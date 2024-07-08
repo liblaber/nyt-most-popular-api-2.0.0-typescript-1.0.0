b0b12c399968613aa62030a8689fc925f9041df7
Cameron Steele <cam.steeleis@gmail.com>
2024-07-08 12:22:26 -0500

    Fix example

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,9 +1,8 @@
 import { GetEmailedPeriodJsonPeriod, NytMostPopular } from 'nyt-most-popular';
 
-
 (async () => {
   const nytMostPopular = new NytMostPopular({
-    apiKey: 'PzqYDMKqGtQgq8ec9ADusKwDXkI9dyn1',
+    apiKey: 'YOUR_API_KEY',
   });
 
   const period: GetEmailedPeriodJsonPeriod = '1';
